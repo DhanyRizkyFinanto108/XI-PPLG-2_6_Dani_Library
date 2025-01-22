c6e42fb77968f2d005a4dd652fc03a9fda108c57
DhanyRizkyFinanto108 <541231048@student.smktelkom-pwt.sch.id>
2025-01-22 14:06:42 +0700

    api baru

--- app/Models/Categori.php
+++ app/Models/Categori.php
@@ -6,5 +6,5 @@ use Illuminate\Database\Eloquent\Model;
 
 class Categori extends Model
 {
-    //
+    protected $fillable = ['name'];
 }
---
+++ routes/api.php
@@ -0,0 +1,6 @@
+<?php
+
+use Illuminate\Support\faredes\Route;
+use App\Http\Controllers\CategoryController;
+
+Route::apResource('categorie', CategoryController::class);
